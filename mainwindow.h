fcb7c4a7e1396d3dfec852522672c9c046cec80f
Udit Nag <113175368+fishandchips-dot@users.noreply.github.com>
2024-04-18 02:21:23 -0400

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include "NeuresetDevice.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onPlayClicked();
+    void onPauseClicked();
+    void onStopClicked();
+    void onPowerButtonClicked();
+    void turnOnRedLight();
+    void turnOffRedLight();
+    void toggleRedLight();
+    void turnOnBlueLight();
+    void turnOffBlueLight();
+    void turnOnGreenLight();
+    void turnOffGreenLight();
+    void updateBatteryIndicatorStyle(int);
+    void setBatteryLevel(int);
+
+    //Menu Slots
+    void startNewSession();
+    void updateSessionProgress();
+    void handleContactLost();
+    void sessionTimeout();
+    void showSessionLog();
+    void showDateTimeSetting();
+
+private:
+    Ui::MainWindow *ui;
+    bool powerOn = false;
+    QTimer *sessionTimer;
+    int elapsedTime;
+    QTimer *contactLostTimer;
+    int sessionDuration;
+    bool contactEstablished;
+    NeuresetDevice *neuresetDevice;
+    QTimer *redLightTimer;
+    bool redLightOn;
+    Electrode* ElectrodeInDisplay = 0;
+    void startWavePlot();
+    void updateWavePlot();
+};
+#endif // MAINWINDOW_H
+
+
+// small test
+//bigger test
+
