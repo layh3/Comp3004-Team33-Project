722a626d3c7b12870ea18c090b5d3d571fce0f5b
Udit Nag <113175368+fishandchips-dot@users.noreply.github.com>
2024-04-19 09:10:12 -0400

    This is ahead of main branch so use this as ur primary
    I cant it merge it to the main branch evry file gets replaced although that would be fine as this is more complete then the main branch. im just giving u more options

--- TermProject/Electrode.cpp
+++ TermProject/Electrode.cpp
@@ -3,18 +3,23 @@
 
 
 
-Electrode::Electrode(int id) : electrodeId(id), isConnected(false), operationTimer(new QTimer(this)) {
+Electrode::Electrode(int id) : electrodeId(id), isConnected(false), disconnectedTimer(new QTimer(this)), operationTimer(new QTimer(this)) {
     // Initialize the electrode with the given ID and as disconnected
-    // initialize the operaation timer
+    // initialize the operation and disconnection timers
 
+    disconnectedTimer->setInterval(3000); // if u change this also cchange the contactlosttimer in Mainwindow
     operationTimer->setInterval(1000);
-    connect(operationTimer, &QTimer::timeout, this, &Electrode::runOperation);
 
+    connect(operationTimer, &QTimer::timeout, this, &Electrode::runOperation);
+    connect(disconnectedTimer, &QTimer::timeout, this, &Electrode::operationEndSequence);
 }
 
 Electrode::~Electrode() {
     // Cleanup tasks if necessary
 }
+
+
+int Electrode::getId(){ return electrodeId; }  // their id is also their index
 
 bool Electrode::checkConnection() {
     // Implement the logic to check if the electrode is properly connected
@@ -39,9 +44,9 @@ void Electrode::disconnectElectrode() {
  void Electrode::initiateOperation() {
 
 
-     opFrequency1 = getRandomDouble(1,4);
-     opFrequency2 = getRandomDouble(4,8);
-     opFrequency3 = getRandomDouble(12,30);
+     opFrequency1 = getRandomDouble(1,4);     // delta band
+     opFrequency2 = getRandomDouble(4,8);     // theta band
+     opFrequency3 = getRandomDouble(12,30);   // beta band
 
      opAmplitude1 = getRandomDouble(0,5);
      opAmplitude1 = getRandomDouble(0,5);
@@ -51,6 +56,12 @@ void Electrode::disconnectElectrode() {
 
      operationTimer->start();
 
+     if(inDisplay){ qInfo("operation started for electrode %d ",electrodeId); }
+
+     //calculate and send intial dominant frequency
+     currentDominatFrequency = calculateDominant();
+     emit dominantFrequency(electrodeId, currentDominatFrequency);
+
  }
 
 
@@ -58,23 +69,127 @@ void Electrode::disconnectElectrode() {
  void Electrode::runOperation() {
 
      if(isConnected){
+
+         disconnectedTimer->stop();
+
          operationTimeElapsed++; // Increment each second
          int seconds = operationTimeElapsed % 60;
 
+         // check time and apply treatment here?
+         applyTreatment();
+
          xGraphForm.append(seconds);
          yGraphForm.append( calculateWaveFormYCoordinate(seconds) );
+
+         if(inDisplay){ qInfo("electrode %d : operation time elapsed =  %d", electrodeId, operationTimeElapsed); }
+
      }
      else{
-         printf("electrode %d is diconnected",electrodeId);
-         operationTimer->stop();
+
+         //  if disconnected start the disconnect timer, if reconnection doesnt occer the operation will end along with the session
+
+         if(inDisplay){ qInfo("electrode %d is diconnected",electrodeId); }
+
+         disconnectedTimer->start();
      }
 
-     if(operationTimeElapsed > 50){
-         operationTimer->stop();
+     if(operationTimeElapsed >= 50){ // operation ends wwith this condition
+
+         //calculate and send final dominant frequency if connected
+         if(isConnected){
+
+             currentDominatFrequency = calculateDominant();
+             emit dominantFrequency(electrodeId, currentDominatFrequency);
+
+         }
+
+         operationEndSequence();
      }
 
 
  }
+
+
+
+
+
+ void Electrode::operationEndSequence(){
+     // reset all operattion  related  variables and datastructures for the next operrationn
+
+     opFrequency1 = 0 ;
+     opFrequency2 = 0;
+     opFrequency3 = 0;
+
+     opAmplitude1 = 0;
+     opAmplitude2 = 0;
+     opAmplitude3 = 0;
+
+
+
+     xGraphForm.clear();
+     yGraphForm.clear();
+
+     operationTimer->stop();
+     disconnectedTimer->stop();
+
+     isConnected = false;
+
+     if(inDisplay){ qInfo("Operation ended for electrode %d",electrodeId); }
+
+ }
+
+
+
+ void Electrode::endOperation(){
+     operationEndSequence();
+ }
+
+
+
+
+
+
+ void Electrode::applyTreatment(){
+
+     // define ur treatment here, also sync the mainwindow ui lights while this happens
+
+     // the goal is to make the current ui graph gradually change its shape over time, but u should doeble check the specs
+     // my idea was to just calculate dominant frequency addd thhe offsset of 5 hertz
+     // then add this new nnumber to all the  frequency bands in this class
+     // while printing the relevant info in terminal
+     // reapeat this 4 times/rounds at 4 different time intervals - if statemnts maybe?
+
+
+
+     // the rest of the my algorithm wwill automatically calulate y coorordinates with ur defined treatment adjustment as it uses the frequency bands in its calculation
+
+ }
+
+
+
+
+
+
+
+ double Electrode::getRandomDouble(double min, double max){
+     return min + QRandomGenerator::global()->generateDouble() * (max - min);
+ };
+
+
+
+
+
+ double Electrode::calculateDominant(){
+
+     if(inDisplay){ qInfo("calculating dominant frequency for electrode %d", electrodeId); }
+
+     double fd = (opFrequency1*opAmplitude1 + opFrequency2*opAmplitude2 + opFrequency3*opAmplitude3) / (opAmplitude1 + opAmplitude2 + opAmplitude3);
+
+     return fd;
+ }
+
+
+
 
 
 
